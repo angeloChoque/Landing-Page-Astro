3d963bb7b5682c50adcc576d09297b416e94c201
angelo <ancricoma_angelo@hotmail.com>
2024-09-06 02:23:07 -0500

    I finished creating the themes button

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://angeloChoquet.github.io",
   base: "Landing-Page-Astro",
-  integrations: [
-    tailwind(),
-    icon({
-      collections: {
-        local: "./src/icons",
-      },
-    }),
-  ],
+  integrations: [tailwind(), icon({
+    collections: {
+      local: "./src/icons"
+    }
+  }), react()]
 });
